4f524bab60e2d10f3d39b426f40829a1999d6433
21programado <21programado@gmail.com>
2026-02-11 14:58:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rompecabezas-v3';
+const CACHE_NAME = 'rompecabezas-v4';
 
 const FILES_TO_CACHE = [
   './',
@@ -22,5 +22,6 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
 
